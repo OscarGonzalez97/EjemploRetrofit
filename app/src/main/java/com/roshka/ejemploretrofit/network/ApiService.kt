4fa2564fcaac7f445c782cbb226c8395297555a1
OscarGonzalez97 <oscaren_gonz0912@hotmail.com>
2022-09-09 02:23:54 -0400

    inicializacion de proyecto

---
+++ app/src/main/java/com/roshka/ejemploretrofit/network/ApiService.kt
@@ -0,0 +1,26 @@
+package com.roshka.ejemploretrofit.network
+
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL = "https://dog.ceo/api/"
+
+private val retrofit = Retrofit.Builder()
+            .addConverterFactory(ScalarsConverterFactory.create()) // agregamos el conversor
+            .baseUrl(BASE_URL) // agregamos la url Base de nuestro service
+            .build() // creamos el objeto retrofit
+
+// creamos la interfaz para que retrofit pueda hablar con nuestro web service usando HTTP
+interface ApiService {
+    @GET("breeds/list/all") // especificamos el endpoint especifico
+    fun getListaRazas(): Call<String> // nos crea un objeto Call, que se utiliza para usar el request
+}
+
+// ya que consume mucho y solo necesitamos una instancia de la API podemos crear un objeto publico para usarlo en todas partes
+object Api {
+    val retrofitService: ApiService by lazy {
+        retrofit.create(ApiService::class.java)
+    }
+}
---
+++ app/src/main/java/com/roshka/ejemploretrofit/ui/main/MainViewModel.kt
@@ -0,0 +1,34 @@
+package com.roshka.ejemploretrofit.ui.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.roshka.ejemploretrofit.network.Api
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainViewModel : ViewModel() {
+    private val _response = MutableLiveData<String>()
+    val response: LiveData<String>
+        get() = _response
+
+    init {
+        getRazas()
+    }
+
+    private fun getRazas() {
+        Api.retrofitService.getListaRazas().enqueue(object: Callback<String>{ // para que lo haga en otro hilo
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                _response.value = response.body()
+            }
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                _response.value = "Failure: " + t.message
+            }
+        })
+
+        _response.value = "xd"
+    }
+
+}
